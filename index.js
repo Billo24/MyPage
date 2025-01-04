4f238c32ae082eb2e41ee9c409b32db92b6edbcd
billo24 <billhorrill@gmail.com>
2025-01-04 15:06:50 +0000

    initial version

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require("http"); 
 const port = 8000; 
 
+// Bill Horrill Web App
+
 http.createServer(function(req, res) { 
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello World!");
